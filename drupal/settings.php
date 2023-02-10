ea92431cfeb043a31f8a9ad44fa6dbc1f0fccf78
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2023-02-10 18:10:57 -0300

    Create settings.php

---
+++ drupal/settings.php
@@ -0,0 +1,4 @@
+<?php 
+$databases['default']['default'] = array ( 'database' => 'sites/default/files/.ht.sqlite', 'prefix' => '', 'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite', 'driver' => 'sqlite', );
+$settings['hash_salt'] = 'qVGLSHb8zzXL6tDZ0xKkQ6Hy-3OmtFoAahqXOwWcaDkWcXl5vLt4xKa0_liP6766QOFTVAUXwA'; 
+$settings['config_sync_directory'] = 'sites/default/files/config_u1ePeTOW5J0m3A701VcC-WK2EY5iTnWGW1L1-02Bf4L3JLVQjRA_otN5oDdIMSIxMbO_sC4tCw/sync';
